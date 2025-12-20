55596c31a921a939d3bde0f14a057302aad643d2
Sittinon Dormae <6810110373@psu.ac.th>
2025-12-20 12:33:30 +0700

    Lab02P3_Authorization

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { UserRole } from 'src/user/entities/user.entity';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  @UseGuards(AuthGuard('jwt'),RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
     return this.bookService.create(createBookDto);
@@ -27,11 +33,15 @@ export class BookController {
     return this.bookService.like(id);
   }
 
+  @UseGuards(AuthGuard('jwt'),RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
     return this.bookService.update(id, updateBookDto);
   }
 
+  @UseGuards(AuthGuard('jwt'),RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.bookService.remove(id);
